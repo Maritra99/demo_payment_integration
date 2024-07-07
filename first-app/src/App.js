3abab9dc04a847f4b0a523f770f1fe7ed712c114
aritra <m1999.aritra@gmail.com>
2024-07-07 22:42:46 +0530

    Testing

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
-export default function App() {
-  return <div>App</div>;
-}
+const Home = () => {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const data = params.get("data");
+
+  return (
+    <div>
+      <h1>Welcome to the Website</h1>
+      {data ? (
+        <>
+          <p>Data from Telegram: {JSON.stringify(data)}</p>
+          <p>Typeof Data from Telegram: {typeof data}</p>
+        </>
+      ) : (
+        <p>No data provided</p>
+      )}
+    </div>
+  );
+};
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
